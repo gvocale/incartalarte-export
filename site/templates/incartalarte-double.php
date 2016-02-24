5ac2d60bf797bed6ea1d220f3904b805940e219f
Giovanni Vocale <gvocale@gmail.com>
2016-02-24 02:06:50 -0500

    still playing around with this loop test

--- site/templates/incartalarte-double.php
+++ site/templates/incartalarte-double.php
@@ -13,62 +13,72 @@ set_time_limit(300); //If set to zero, no time limit is imposed. ?>
 
 <h4>Posts <strong><?php echo $posts->count() ?></strong></h4>
 
-<?php $filtered = $posts->filterBy('ID',"15230") ?>
-<?php foreach ($filtered as $post): ?>
-		<li style="margin-top: 1rem">
-			<?php print_r($post) ?>
-		</li>
+<?php $post = $posts->filterBy('ID',"15230")->first() ?>
+<?php foreach ($post as $field => $value): ?>
+	<li style="margin-top: 1rem">
+		<strong><?php echo $field ?></strong> <?php echo $value ?>
+	</li>
 <?php endforeach ?>
 
-<?php $filtered = $posts->filterBy('post_parent',"15230") ?>
-<?php foreach ($filtered as $post): ?>
-		<li style="margin-top: 1rem">
-			<?php print_r($post) ?>
-		</li>
+<hr style="height:10px; width: 100%; background: red; margin: 20px">
+
+<?php $post_parent = $posts->filterBy('post_parent',$post->ID())->first() ?>
+<?php foreach ($post_parent as $field => $value): ?>
+	<li style="margin-top: 1rem">
+		<strong><?php echo $field ?></strong> <?php echo $value ?>
+	</li>
 <?php endforeach ?>
+
+<hr style="height:10px; width: 100%; background: red; margin: 20px">
 
 <h4>term_relationships <strong><?php echo $term_relationships->count() ?></strong></h4>
 
-<?php $filtered = $term_relationships->filterBy('object_id',"15230") ?>
-<?php foreach ($filtered as $post): ?>
+<?php $term_relationships = $term_relationships->filterBy('object_id',$post->ID()) ?>
+<?php foreach ($term_relationships as $term_relationship): ?>
+	<?php foreach ($term_relationship as $field => $value): ?>
 		<li style="margin-top: 1rem">
-			<?php print_r($post) ?>
+			<strong><?php echo $field ?></strong> <?php echo $value ?>
+			<?php $terms = $terms->filterBy('term_id',$term_relationship->term_taxonomy_id()) ?>
+			<?php print_r($terms->first()) ?>
 		</li>
+	<?php endforeach ?>
 <?php endforeach ?>
+
+<hr style="height:10px; width: 100%; background: red; margin: 20px">
 
 
 <h4>Terms <strong><?php echo $terms->count() ?></strong></h4>
 
-<?php $filtered = $terms->filterBy('term_id',"2") ?>
+<?php $terms = $terms->filterBy('term_id',"2") ?>
 <?php foreach ($filtered as $post): ?>
-		<li style="margin-top: 1rem">
-			<?php print_r($post) ?>
-		</li>
+	<li style="margin-top: 1rem">
+		<?php print_r($post) ?>
+	</li>
 <?php endforeach ?>
 
 
 <?php $filtered = $terms->filterBy('term_id',"685") ?>
 <?php foreach ($filtered as $post): ?>
-		<li style="margin-top: 1rem">
-			<?php print_r($post) ?>
-		</li>
+	<li style="margin-top: 1rem">
+		<?php print_r($post) ?>
+	</li>
 <?php endforeach ?>
 
 
 <?php $filtered = $terms->filterBy('term_id',"688") ?>
 <?php foreach ($filtered as $post): ?>
-		<li style="margin-top: 1rem">
-			<?php print_r($post) ?>
-		</li>
+	<li style="margin-top: 1rem">
+		<?php print_r($post) ?>
+	</li>
 <?php endforeach ?>
 
 <h4>postmeta <strong><?php echo $postmeta->count() ?></strong></h4>
 
 <?php $filtered = $postmeta->filterBy('post_id',"15230") ?>
 <?php foreach ($filtered as $post): ?>
-		<li style="margin-top: 1rem">
-			<?php print_r($post) ?>
-		</li>
+	<li style="margin-top: 1rem">
+		<?php print_r($post) ?>
+	</li>
 <?php endforeach ?>
 
 <?php snippet('footer') ?>
--- site/templates/incartalarte-loop.php
+++ site/templates/incartalarte-loop.php
@@ -15,113 +15,98 @@ ini_set('max_execution_time', 300); //300 seconds = 5 minutes
 //OR
 set_time_limit(300); //If set to zero, no time limit is imposed. ?>
 
-<?php foreach ($posts->limit(100) as $counter => $post): ?>
-	
-	<li style="margin-top: 1rem">
-		<?php if($post[ID]->isNotEmpty()): ?>
+<?php foreach ($posts as $counter => $post): ?>
+	<?php if ($counter > 0 AND $counter < 1000 AND $post->post_type != "attachment" AND $post->post_type != "page" ): ?>
+		<li style="margin-top: 1rem">
 			<p><strong>ID</strong> <?php echo $post->ID() ?></p>
-		<?php endif ?>
-		<?php if($post->ID()->isNotEmpty()): ?>
 			<p><strong>post_title</strong> <?php echo $post->post_title(); ?>	</p>
-		<?php endif ?>
-		<?php if($post->ID()->isNotEmpty()): ?>
-
 			<p><strong>post_content</strong> <?php echo $post->post_content(); ?>	</p>
-		<?php endif ?>
-		<?php if($post->ID()->isNotEmpty()): ?>
 			<p><strong>post_name</strong> <?php echo $post->post_name(); ?>	</p>
-		<?php endif ?>
-		<?php if($post->ID()->isNotEmpty()): ?>
 			<p><strong>guid</strong> <?php echo $post->guid(); ?>	</p>
-		<?php endif ?>
-		<?php if($post->ID()->isNotEmpty()): ?>
 			<p><strong>post_type</strong> <?php echo $post->post_type(); ?>	</p>
-		<?php endif ?>
-		<?php if($post->ID()->isNotEmpty()): ?>
 			<p><strong>post_parent</strong> <?php echo $post->post_parent(); ?>	</p>
-		<?php endif ?>
+			
+			<?php $term_id = $term_relationships->filterBy('object_id', $post->ID())->first()->object_id(); ?>
+			<?php $term_taxonomy_id = $term_relationships->filterBy('object_id', $post->ID())->first()->term_taxonomy_id(); ?>
+			<p><strong>term_id</strong> <?php echo $term_id ?>	</p>
+			<p><strong>term_taxonomy_id</strong> <?php echo $term_taxonomy_id ?>	</p>
+			<?php $term_count = $terms->count() ?>
+			<p><strong>term_count </strong> <?php echo $term_count ?></p>
+			<p><strong>Category Name </strong><?php $category_name = $terms->filterBy('term_id',$term_taxonomy_id)->first()->name() ?><?php echo $category_name ?></p>
+			<p><strong>Category Slug </strong><?php $category_slug = $terms->filterBy('term_id',$term_taxonomy_id)->first()->slug() ?><?php echo $category_slug ?></p>
+			<?php foreach ($terms->filterBy('term_id',$term_taxonomy_id) as $term): ?>
+				<p><strong>Categoria </strong><?php echo $term->name ?> <?php echo $term->slug ?> </p>
+			<?php endforeach ?>
+			<?php foreach ($postmeta->filterBy('post_id',$term_taxonomy_id) as $meta): ?>
+				<?php print_r($meta);?>
+			<?php endforeach ?>
+			<?php foreach ($posts->filterBy('post_parent',$post->ID()) as $image): ?>
+				<p><strong>image url with doulbe // </strong><?php echo $image->guid() ?></p>
+			<?php endforeach ?>
 
-		<?php $term_id = $term_relationships->filterBy('object_id', $post->ID())->first()->object_id(); ?>
-		<?php $term_taxonomy_id = $term_relationships->filterBy('object_id', $post->ID())->first()->term_taxonomy_id(); ?>
-		<p><strong>term_id</strong> <?php echo $term_id ?>	</p>
-		<p><strong>term_taxonomy_id</strong> <?php echo $term_taxonomy_id ?>	</p>
-		<?php $term_count = $terms->count() ?>
-		<p><strong>term_count </strong> <?php echo $term_count ?></p>
-		<p><strong>Category Name </strong><?php $category_name = $terms->filterBy('term_id',$term_taxonomy_id)->first()->name() ?><?php echo $category_name ?></p>
-		<p><strong>Category Slug </strong><?php $category_slug = $terms->filterBy('term_id',$term_taxonomy_id)->first()->slug() ?><?php echo $category_slug ?></p>
-		<?php foreach ($terms->filterBy('term_id',$term_taxonomy_id) as $term): ?>
-			<p><strong>Categoria </strong><?php echo $term->name ?> <?php echo $term->slug ?> </p>
-		<?php endforeach ?>
-		<?php foreach ($postmeta->filterBy('post_id',$term_taxonomy_id) as $meta): ?>
-			<?php print_r($meta);?>
-		<?php endforeach ?>
-		<?php foreach ($posts->filterBy('post_parent',$post->ID()) as $image): ?>
-			<p><strong>image url with doulbe // </strong><?php echo $image->guid() ?></p>
-		<?php endforeach ?>
-
-		<?php foreach ($postmeta->filterBy('post_id',$post->ID()) as $image): ?>
-			<p><strong>image url </strong><?php print_r($image) ?></p>
-		<?php endforeach ?>
-		<hr style="height: 2px; background-color: red; width: 100%">
-	</li>
-	<?php try {
+			<?php foreach ($postmeta->filterBy('post_id',$post->ID()) as $image): ?>
+				<p><strong>image url </strong><?php print_r($image) ?></p>
+			<?php endforeach ?>
+			<hr style="height: 2px; background-color: red; width: 100%">
+		</li>
+		<?php try {
 
 			// Remove <img> tag from post content
-		$img_tag = '/<img(.*)\/>/';
-		$img_tag_replacement = "";
-		preg_match_all($img_tag, $post->post_content(), $img_tag_matches);
-		$cleaned_post_content = str_replace($img_tag_matches[0], $img_tag_replacement, $post->post_content());
+			$img_tag = '/<img(.*)\/>/';
+			$img_tag_replacement = "";
+			preg_match_all($img_tag, $post->post_content(), $img_tag_matches);
+			$cleaned_post_content = str_replace($img_tag_matches[0], $img_tag_replacement, $post->post_content());
 
-		$newPage = page('collezione')->children()->create($post->post_name(), 'project', array(
-			'id'     => $post->ID(),
-			'guid' => $post->post_name(),
-			'wordpress-guid'      => $post->guid(),
-			'Titolo'      => $post->post_title(),
-			'Descrizione'      => $cleaned_post_content,
-			'Tag'      => $category_name,
-			));
-		echo "The page " . $post->post_title() . " has been created \n";
-		echo $newPage->root() . "\n";
+			$newPage = page('collezione')->children()->create($post->post_name(), 'project', array(
+				'id'     => $post->ID(),
+				'guid' => $post->post_name(),
+				'wordpress-guid'      => $post->guid(),
+				'Titolo'      => $post->post_title(),
+				'Descrizione'      => $cleaned_post_content,
+				'Tag'      => $category_name,
+				));
+			echo "The page " . $post->post_title() . " has been created \n";
+			echo $newPage->root() . "\n";
 
 			// Save and rename all images linked in content
-		$img_url = '/http.*jpg/';
-		preg_match_all($img_url, $post->post_content, $img_url_matches);
-		$n = 0;
+			$img_url = '/http.*jpg/';
+			preg_match_all($img_url, $post->post_content, $img_url_matches);
+			$n = 0;
 
 			// Save main image
-		foreach ($posts->filterBy('post_parent',$post->ID()) as $image) {
-			echo $image->guid() . " is being saved \n";
-			$n++;
-			file_put_contents($newPage->root() . DS . $post->post_name() . "-" . $n . ".jpg", fopen($image->guid(), 'r'));
-		};
+			foreach ($posts->filterBy('post_parent',$post->ID()) as $image) {
+				echo $image->guid() . " is being saved \n";
+				$n++;
+				file_put_contents($newPage->root() . DS . $post->post_name() . "-" . $n . ".jpg", fopen($image->guid(), 'r'));
+			};
 
 			// Save main image from altrnate link in post meta
 
-		foreach ($postmeta->filterBy('post_id',$post->ID()) as $image) {
-			$image_url = "http://www.incartalarte.it/wp-content/uploads/" . $image->meta_value();
-			echo $image_url . " is being saved \n";
-			$n++;
-			file_put_contents($newPage->root() . DS . $post->post_name() . "-" . $n . ".jpg", fopen($image_url, 'r'));
-		};
+			foreach ($postmeta->filterBy('post_id',$post->ID()) as $image) {
+				$image_url = "http://www.incartalarte.it/wp-content/uploads/" . $image->meta_value();
+				echo $image_url . " is being saved \n";
+				$n++;
+				file_put_contents($newPage->root() . DS . $post->post_name() . "-" . $n . ".jpg", fopen($image_url, 'r'));
+			};
 
 			// Save other images linked into content
-		foreach ($img_url_matches[0] as $img_url_match) {
-			echo $img_url_match . " is being saved \n";
-			$n++;
-			file_put_contents($newPage->root() . DS . $post->post_name() . "-" . $n . ".jpg", fopen($img_url_match, 'r'));
+			foreach ($img_url_matches[0] as $img_url_match) {
+				echo $img_url_match . " is being saved \n";
+				$n++;
+				file_put_contents($newPage->root() . DS . $post->post_name() . "-" . $n . ".jpg", fopen($img_url_match, 'r'));
+			};
+
+			
+		}
+
+		catch(Exception $e) {
+
+			echo "The page " . $post->post_title() . " has not been created <br><br>";
+  				// optional error message: $e->getMessage();
 		};
 
-
-	}
-
-	catch(Exception $e) {
-
-		echo "The page " . $post->post_title() . " has not been created <br><br>";
-  				// optional error message: $e->getMessage();
-	};
-
-	?>
-
+		?>
+	<?php endif ?>
 <?php endforeach ?>
 
 
