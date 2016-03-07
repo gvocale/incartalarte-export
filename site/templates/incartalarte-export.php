f2d608778e499a6f4e680bb97a8878a0835065b3
Giovanni Vocale <gvocale@gmail.com>
2016-03-07 02:23:04 -0500

    attachment still not working

--- site/templates/incartalarte-export.php
+++ site/templates/incartalarte-export.php
@@ -4,77 +4,134 @@
 <?php $posts = db::select('alfa_posts', '*'); ?>
 <?php $term_relationships = db::select('alfa_term_relationships', '*'); ?>
 <?php $terms = db::select('alfa_terms', '*'); ?>
+<?php $terms_array = (array) $terms ?>
+<?php $terms_array = $terms_array['data'] ?>
 
 <?php 
 ini_set('max_execution_time', 300); //300 seconds = 5 minutes
 //OR
 set_time_limit(300); //If set to zero, no time limit is imposed. ?>
 
+<?php $new_post = [] ?>
+
 <!-- Post (contenuto del post) -->
 <?php $post = $posts->filterBy('ID',"9357")->first() ?>
-
-<!-- <?php echo '<pre>'; print_r($post); echo '</pre>'; ?> -->
-
-<hr class="" style="height: 1px; width: 100%; background:red; margin: 2rem 0;"></hr>
-
 <?php foreach ($post as $key => $value): ?>
-	<?php if(!empty($value)): ?>
-		<li><strong><?php echo $key ?> </strong><?php echo $value ?></li>
-	<?php endif ?>
+	<?php $new_post["$key"] = "$value"; ?>
 <?php endforeach ?>
+
+<?php echo '<pre>'; print_r($new_post); echo '</pre>'; ?>
+
 <hr class="" style="height: 1px; width: 100%; background:gray; margin: 2rem 0;"></hr>
-<!-- Terms Relationship (codice categoria) -->
-<?php $term_relationships = $term_relationships->filterBy('object_id',"9357") ?>
-<?php foreach ($term_relationships as $term_relationship): ?>
-	<li><strong>term_taxonomy_id </strong><?php echo $term_relationship->term_taxonomy_id() ?></li>
-	<!-- Terms (nome categoria) -->
-	<?php $term = $terms->filterBy('term_id',$term_relationship->term_taxonomy_id())->first() ?>
-	<!-- <?php echo '<pre>'; print_r($term); echo '</pre>'; ?> -->
-	<li><strong>name </strong><?php echo $term->name() ?></li>
-	<li><strong>slug </strong><?php echo $term->slug() ?></li>
-	<hr class="" style="height: 1px; width: 100%; background:gray; margin: 2rem 0;"></hr>
+
+
+<!-- Terms Relationship (Category Number) -->
+<?php $term_relationships = $term_relationships->filterBy('object_id',$post->ID()) ?>
+<?php $term_relationships_array = (array) $term_relationships ?>
+<?php $term_relationships_array = $term_relationships_array['data'] ?>
+
+
+<?php $category_number = [] ?>
+<?php $category_name = [] ?>
+<?php $category_slug = [] ?>
+<?php foreach ($term_relationships_array as $category): ?>
+	<?php $category_number[] = $category->term_taxonomy_id() ?>
 <?php endforeach ?>
+<p><strong>Category Number</strong></p>
+<?php echo '<pre>'; print_r($category_number); echo '</pre>'; ?>
+
+<?php $category_number_value =  rtrim(implode(',', $category_number), ','); ?>
+
+<?php $new_post["category_number"] = "$category_number_value" ?>
+
+
+<hr class="" style="height: 1px; width: 100%; background:gray; margin: 2rem 0;"></hr>
+
+<p><strong>Category Name & slug</strong></p>
+
+<?php foreach ($category_number as $each): ?>
+	<?php $term = $terms->filterBy('term_id', $each) ?>
+	<?php $term = $term->first() ?>
+	<?php $category_name[] = $term->name() ?>
+	<?php $category_slug[] = $term->slug() ?>	
+<?php endforeach ?>
+
+<?php echo '<pre>'; print_r($category_name); echo '</pre>'; ?>
+<?php echo '<pre>'; print_r($category_slug); echo '</pre>'; ?>
+
+<?php $category_name_value =  rtrim(implode(',', $category_name), ','); ?>
+
+<?php $new_post["category_name"] = "$category_name_value" ?>
+
+<?php $category_slug_value =  rtrim(implode(',', $category_slug), ','); ?>
+
+<?php $new_post["category_slug"] = "$category_slug_value" ?>
+
+<?php echo '<pre>'; print_r($new_post); echo '</pre>'; ?>
+
+<hr class="" style="height: 1px; width: 100%; background:gray; margin: 2rem 0;"></hr>
+
 <!-- Post meta (informazioni varie) -->
-<?php $metas = $postmeta->filterBy('post_id',"9357") ?>
+<?php $metas = $postmeta->filterBy('post_id',$post->ID()) ?>
 <?php foreach ($metas as $meta): ?>
-	<!-- <?php echo '<pre>'; print_r($meta); echo '</pre>'; ?> -->
-	<?php if(!empty($meta->meta_value())): ?>
-		<li><strong><?php echo $meta->meta_key() ?> </strong><?php echo $meta->meta_value() ?></li>
-		<hr class="" style="height: 1px; width: 100%; background:gray; margin: 2rem 0;"></hr>
-	<?php endif ?>
+	<?php $new_post["$meta->meta_key()"] = "$meta->meta_value()"; ?>
 <?php endforeach ?>
+
+<p><strong>Post Meta added to $new_post</strong></p>
+
+<?php echo '<pre>'; print_r($new_post); echo '</pre>'; ?>
+
 <hr class="" style="height: 1px; width: 100%; background:red; margin: 2rem 0;"></hr>
 
-<!-- Post (contenuto del post) -->
-<?php $post = $posts->filterBy('ID',"9358") ?>
-<?php echo '<pre>'; print_r($post); echo '</pre>'; ?>
-<?php foreach ($post as $key => $value): ?>
-	<?php if(!empty($value)): ?>
-		<li><strong><?php echo $key ?> </strong><?php echo $value ?></li>
-	<?php endif ?>
+
+<!-- Post Attachment -->
+<?php $post_attachment = $posts->filterBy('ID',"9358") ?>
+<?php $attachment = (array) $post_attachment->first(); ?>
+<?php echo '<pre>'; print_r($attachment); echo '</pre>'; ?>
+
+<?php foreach ($attachment as $attachment_each): ?>
+	<?php $new_post["attachment_post_title"] = "$attachment_each->post_title()"; ?>
+	<?php $new_post["attachment_post_name"] = "$attachment_each->post_name()"; ?>
+	<?php $new_post["attachment_guid"] = "$attachment_each->guid()"; ?>
 <?php endforeach ?>
-<hr class="" style="height: 1px; width: 100%; background:gray; margin: 2rem 0;"></hr>
-<!-- Terms Relationship (codice categoria) -->
-<?php $term_relationships = $term_relationships->filterBy('object_id',"9357") ?>
-<?php foreach ($term_relationships as $term_relationship): ?>
-	<li><strong>term_taxonomy_id </strong><?php echo $term_relationship->term_taxonomy_id() ?></li>
-	<!-- Terms (nome categoria) -->
-	<?php $term = $terms->filterBy('term_id',$term_relationship->term_taxonomy_id())->first() ?>
-	<!-- <?php echo '<pre>'; print_r($term); echo '</pre>'; ?> -->
-	<li><strong>name </strong><?php echo $term->name() ?></li>
-	<li><strong>slug </strong><?php echo $term->slug() ?></li>
-	<hr class="" style="height: 1px; width: 100%; background:gray; margin: 2rem 0;"></hr>
-<?php endforeach ?>
-<!-- Post meta (informazioni varie) -->
-<?php $metas = $postmeta->filterBy('post_id',"9357") ?>
-<?php foreach ($metas as $meta): ?>
-	<!-- <?php echo '<pre>'; print_r($meta); echo '</pre>'; ?> -->
-	<?php if(!empty($meta->meta_value())): ?>
-		<li><strong><?php echo $meta->meta_key() ?> </strong><?php echo $meta->meta_value() ?></li>
-		<hr class="" style="height: 1px; width: 100%; background:gray; margin: 2rem 0;"></hr>
-	<?php endif ?>
-<?php endforeach ?>
+
 <hr class="" style="height: 1px; width: 100%; background:red; margin: 2rem 0;"></hr>
 
+<?php echo '<pre>'; print_r($new_post); echo '</pre>'; ?>
+
+<?php try {
+
+	// Remove <img> tag from post content
+	$img_tag = '/<img(.*)\/>/';
+	$img_tag_replacement = "";
+	preg_match_all($img_tag, $post->post_content(), $img_tag_matches);
+	$cleaned_post_content = str_replace($img_tag_matches[0], $img_tag_replacement, $post->post_content());
+
+	$newPage = page('incartalarte-export')->children()->create($new_post->post_name(), 'project', $new_post);
+	echo "The page " . $new_post->post_name() . " has been created \n";
+	echo $newPage->root() . "\n";
+
+					// Save and rename all images linked in content
+	$img_url = '/http.*jpg/';
+	preg_match_all($img_url, $post->post_content, $img_url_matches);
+	$n = 0;
+	foreach ($img_url_matches[0] as $img_url_match) {
+		echo $img_url_match . " is being saved \n";
+		$n++;
+		file_put_contents($newPage->root() . DS . $post->post_name() . "-" . $n . ".jpg", fopen($img_url_match, 'r'));
+	};
+}
+
+catch(Exception $e) {
+
+	echo $e->getMessage();
+
+	echo $e;
+
+	echo "The page " . $new_post->post_name() . " has not been created <br><br>";
+  				// optional error message: $e->getMessage();
+}; ?>
+
+<?php echo '<pre>'; print_r($newPage); echo '</pre>'; ?>
 
 <?php snippet('footer') ?>
